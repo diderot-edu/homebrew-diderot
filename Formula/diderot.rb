9cd023088ec65b566120670b3c21fe29fc75896a
drbarrett <drbarrett@cmu.edu>
2020-05-13 11:36:04 -0400

    Updated comments

--- Formula/diderot.rb
+++ Formula/diderot.rb
@@ -1,9 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Diderot < Formula
   desc "Diderot Compiler"
   homepage "https://github.com/diderot-edu/diderot-guide/"
+
+  # The next 2 lines need to changr whenever we upload a new version of dc
   url "https://diderot-dist.s3.amazonaws.com/dc-macos-0.16.6.zip"
   sha256 "56bf36b83db5316aace7fbd7c09d6766b41cadde8404f12596a1e640fbdd8188"
 
