23fb9cff5e7d27db7d27f9934afb23d03a18e04d
drbarrett <drbarrett@cmu.edu>
2020-05-13 13:02:38 -0400

    added texel

--- Formula/diderot.rb
+++ Formula/diderot.rb
@@ -6,12 +6,13 @@ class Diderot < Formula
 
   # The next 2 lines need to change whenever we upload a new version of dc
   url "https://diderot-dist.s3.amazonaws.com/dc-macos-0.16.6.zip"
-  sha256 "56bf36b83db5316aace7fbd7c09d6766b41cadde8404f12596a1e640fbdd8188"
+  sha256 "8bf799bb24fba4bdc622a34d53263db9ff92328d05784174a51a9ae7010083cc"
 
   depends_on "pandoc"
 
   def install
     bin.install "dc"
+    bin.install "texel"
   end
 
   test do
