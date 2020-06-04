e2e13091c8db504d734a1e6b6df2e90b6da327bf
drbarrett <drbarrett@cmu.edu>
2020-06-04 11:58:51 -0400

    updated version to 0.16.7

--- Formula/diderot.rb
+++ Formula/diderot.rb
@@ -5,8 +5,8 @@ class Diderot < Formula
   homepage "https://github.com/diderot-edu/diderot-guide/"
 
   # The next 2 lines need to change whenever we upload a new version of dc
-  url "https://diderot-dist.s3.amazonaws.com/dc-macos-0.16.6.zip"
-  sha256 "167fd72bd266644f9a1cf3a47193a58f1c2645a3e0f78eee8061a615f38a5aae"
+  url "https://diderot-dist.s3.amazonaws.com/dc-macos-0.16.7.zip"
+  sha256 "49532efe3b258e9718cb4305740d7c112c0652fab413ca9e47a70999f4511526"
 
   depends_on "pandoc"
 
